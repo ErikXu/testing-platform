622222a8c70e2d8ffdce46537024e31dbf194d06
Erik Xu <81636759@qq.com>
2022-07-14 17:28:20 +0800

    Add pack agent.

---
+++ src/pack_agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_NAME=${IMAGE_NAME:-agent}
+echo "IMAGE_NAME: "$IMAGE_NAME
+
+IMAGE_TAG=${IMAGE_TAG:-1.0.0}
+echo "IMAGE_TAG: "$IMAGE_TAG
+
+
+docker build --no-cache --disable-content-trust=true -t $IMAGE_NAME:${IMAGE_TAG} -f ../docker/Dockerfile_Agent ./publish_agent/
+#docker push ${IMAGE_NAME}:${IMAGE_TAG}
+#docker rmi ${IMAGE_NAME}:${IMAGE_TAG}
